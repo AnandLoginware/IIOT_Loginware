b0018e35410ab9dae6a4c4991a7c537766df3b4e
AnandK20 <anand.9k7@gmail.com>
2021-03-20 10:58:04 +0530

    adminEndpoints updated

--- adminEndpoints.py
+++ adminEndpoints.py
@@ -35,3 +35,31 @@ def serverConfiguration():
   except Exception as e:
       print(e)   
       return jsonify({"result" : {"message" : "something went wrong", "status" : 0}})
+      
+@admin.route('/updateNetworkDetails', methods = ['POST'])
+def UpdatenetworkDetails():
+    ip = request.get_json()['ip']
+    gateway = request.get_json()['gateway']
+    dns = request.get_json()['dns']
+    networkFileData = "interface eth0 \n static ip_address = {}\n static routers = {}\n static domain_name_servers = {}".format(ip, gateway, dns)
+    print(networkFileData)
+    try:
+        result = networkConf.query.filter_by(id = 1).scalar()
+        if result != None:
+            db.session.query(networkConf).filter(networkConf.id == 1).update({"ip" : ip, "gateway" : gateway, "dns" : dns})
+            db.session.commit()
+            with open('/etc/dhcpcd.conf', 'w') as f:
+                f.write(networkFileData)
+                f.close()
+            return jsonify({"result" : {"status" : 1, "message" : "Network details updated successfully"}})
+        else:
+            networkConfObject = networkConf(ip = ip, gateway = gateway, dns = dns)
+            db.session.add(networkConfObject)
+            db.session.commit()
+            with open('/etc/dhcpcd.conf', 'w') as f:
+                f.write(networkFileData)
+                f.close()
+            return jsonify({"result" : {"status" : 1, "message" : "Network details saved successfully"}})
+    except Exception as e:
+        return jsonify({"result" : {"status" : 0, "message" : Something went wrong"}})
+        
